2152e8af5e870988f1067096d495f42cb393f0c9
Josa Gesell <josa@gesell.me>
2022-10-11 20:42:52 +0200

    wip: simplify config

--- lua/jg/ui/input.lua
+++ lua/jg/ui/input.lua
@@ -30,61 +30,51 @@ local map_util = {
   end,
 }
 
-local global_config = {
-  get_mod_config = function()
-    return {
-      -- Default prompt string
-      default_prompt = 'Input:',
+local config = {
+  -- Default prompt string
+  default_prompt = 'Input:',
 
-      -- Can be 'left', 'right', or 'center'
-      prompt_align = 'left',
+  -- Can be 'left', 'right', or 'center'
+  prompt_align = 'left',
 
-      -- When true, <Esc> will close the modal
-      insert_only = true,
+  -- When true, <Esc> will close the modal
+  insert_only = true,
 
-      -- When true, input will start in insert mode.
-      start_in_insert = true,
+  -- When true, input will start in insert mode.
+  start_in_insert = true,
 
-      -- These are passed to nvim_open_win
-      anchor = 'SW',
-      border = 'rounded',
-      -- 'editor' and 'win' will default to being centered
-      relative = 'cursor',
+  -- These are passed to nvim_open_win
+  anchor = 'SW',
+  border = 'rounded',
+  -- 'editor' and 'win' will default to being centered
+  relative = 'cursor',
 
-      -- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
-      prefer_width = 40,
-      width = nil,
-      -- min_width and max_width can be a list of mixed types.
-      -- min_width = {20, 0.2} means "the greater of 20 columns or 20% of total"
-      max_width = { 140, 0.9 },
-      min_width = { 20, 0.2 },
+  -- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+  prefer_width = 40,
+  width = nil,
+  -- min_width and max_width can be a list of mixed types.
+  -- min_width = {20, 0.2} means "the greater of 20 columns or 20% of total"
+  max_width = { 140, 0.9 },
+  min_width = { 20, 0.2 },
 
-      -- Window transparency (0-100)
-      winblend = 10,
-      -- Change default highlight groups (see :help winhl)
-      winhighlight = '',
+  -- Window transparency (0-100)
+  winblend = 10,
+  -- Change default highlight groups (see :help winhl)
+  winhighlight = '',
 
-      -- Set to `false` to disable
-      mappings = {
-        n = {
-          ['<Esc>'] = 'Close',
-          ['<CR>'] = 'Confirm',
-        },
-        i = {
-          ['<C-c>'] = 'Close',
-          ['<CR>'] = 'Confirm',
-          ['<Up>'] = 'HistoryPrev',
-          ['<Down>'] = 'HistoryNext',
-        },
-      },
-
-      override = function(conf)
-        -- This is the config that will be passed to nvim_open_win.
-        -- Change values here to customize the layout
-        return conf
-      end,
-    }
-  end,
+  -- Set to `false` to disable
+  mappings = {
+    n = {
+      ['<Esc>'] = 'Close',
+      ['<CR>'] = 'Confirm',
+    },
+    i = {
+      ['<C-c>'] = 'Close',
+      ['<CR>'] = 'Confirm',
+      ['<Up>'] = 'HistoryPrev',
+      ['<Down>'] = 'HistoryNext',
+    },
+  },
 }
 -- local patch = require('dressing.patch')
 
@@ -495,8 +485,6 @@ local function create_or_update_win(config, prompt, opts)
     end
   end
 
-  winopt = config.override(winopt) or winopt
-
   -- If the floating win was already open
   if win_conf then
     -- Make sure the previous on_confirm callback is called with nil
@@ -523,10 +511,7 @@ setmetatable(M, {
     if type(opts) ~= 'table' then
       opts = { prompt = tostring(opts) }
     end
-    local config = global_config.get_mod_config()
-    -- if not config.enabled then
-    --   return patch.original_mods.input(opts, on_confirm)
-    -- end
+
     if vim.fn.hlID('DressingInputText') ~= 0 then
       vim.notify(
         'DressingInputText highlight group is deprecated. Set winhighlight="NormalFloat:MyHighlightGroup" instead',
