fbfc4c0dcf9a6038bb94c1a81172aad4dc04b6d6
Josa Gesell <josa@gesell.me>
2022-10-13 09:23:42 +0200

    feat: use telescope as select

--- lua/jg/ui.lua
+++ lua/jg/ui.lua
@@ -2,6 +2,7 @@ local M = {}
 
 function M.setup()
   vim.ui.input = require('jg.ui.input')
+  vim.ui.select = require('jg.ui.select')
 end
 
 return M
--- lua/jg/ui/select.lua
+++ lua/jg/ui/select.lua
@@ -1,10 +1,4 @@
-local M = {}
-
-function M.setup()
-  vim.ui.select = M.select
-end
-
-function M.select(items, opts, on_choice)
+local function select(items, opts, on_choice)
   local action_set = require('telescope.actions.set')
   local actions = require('telescope.actions')
   local pickers = require('telescope.pickers')
@@ -15,31 +9,33 @@ function M.select(items, opts, on_choice)
   opts = opts or {}
   on_choice = on_choice or function() end
 
-  pickers.new({
-    prompt_title = opts.prompt or '',
-    finder = finders.new_table({
-      results = items,
-      entry_maker = function(item)
-        local text = (opts.format_item or tostring)(item)
-        return { display = text, ordinal = text, value = item }
-      end,
-    }),
-    sorter = conf.generic_sorter(),
-    layout_strategy = 'horizontal',
-    layout_config = {
-      horizontal = { width = 60, height = 16 },
-    },
-    results_title = false,
-    attach_mappings = function()
-      action_set.select:replace(function(prompt_bufnr)
-        actions.close(prompt_bufnr, false)
-        local selected = state.get_selected_entry() or {}
-        on_choice(selected.value, selected.index)
-      end)
+  pickers
+    .new({
+      prompt_title = opts.prompt or '',
+      finder = finders.new_table({
+        results = items,
+        entry_maker = function(item)
+          local text = (opts.format_item or tostring)(item)
+          return { display = text, ordinal = text, value = item }
+        end,
+      }),
+      sorter = conf.generic_sorter(),
+      layout_strategy = 'horizontal',
+      layout_config = {
+        horizontal = { width = 60, height = 16 },
+      },
+      results_title = false,
+      attach_mappings = function()
+        action_set.select:replace(function(prompt_bufnr)
+          actions.close(prompt_bufnr, false)
+          local selected = state.get_selected_entry() or {}
+          on_choice(selected.value, selected.index)
+        end)
 
-      return true
-    end,
-  }):find()
+        return true
+      end,
+    })
+    :find()
 end
 
-return M
+return select
